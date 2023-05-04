df441abb76dff98ee532f9b560689975e2ee74f7
anthony <anthonyspiteri@hotmail.fr>
2023-05-04 14:39:12 +0200

    securiser upload image

--- src/Form/ProduitType.php
+++ src/Form/ProduitType.php
@@ -6,7 +6,10 @@ use App\Entity\Produit;
 use App\Entity\Categorie;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\All;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Validator\Constraints\File;
+use Symfony\Component\Validator\Constraints\Count;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -20,12 +23,31 @@ class ProduitType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         
+        
         $builder
         //mapped false pour ne pas le lier a la base de données
             ->add('images', FileType::class, [
                 'label' => 'Photos',
                 'multiple' => true,
                 'mapped' => false,
+                'required' => false,
+                'constraints' => [
+                    new Count([
+                        'max' => 4,
+                        'maxMessage' => 'Vous ne pouvez télécharger que {{ limit }} images maximum',
+                    ]),
+        new All([
+            new File([
+                'maxSize' => '1024k',
+                'maxSizeMessage' => 'La taille maximale autorisée est de {{ limit }}.',
+                'mimeTypes' => [
+                    'image/jpeg',
+                    'image/png'
+                ],
+                'mimeTypesMessage' => 'Veuillez télécharger une image valide (JPEG, PNG, GIF)',
+            ])
+        ])
+                ],
             ])
             ->add('nomProduit', TextType::class, [
                 'label' => 'Nom du produit'
