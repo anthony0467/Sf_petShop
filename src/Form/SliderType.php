3bb86251698af4c75c7007c447630a0927f1d53a
anthony <anthonyspiteri@hotmail.fr>
2023-07-06 14:49:05 +0200

    feat: sliderType form

---
+++ src/Form/SliderType.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Slider;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\All;
+use Symfony\Component\Validator\Constraints\Url;
+use Symfony\Component\Validator\Constraints\File;
+use Symfony\Component\Validator\Constraints\Count;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class SliderType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title', TextType::class, [
+                'required' => true,
+            ])
+            ->add('images', FileType::class, [
+                'label' => 'Photos',
+                'multiple' => true,
+                'mapped' => false,
+                'required' => true,
+                'constraints' => [
+                    new Count([
+                        'max' => 1,
+                        'maxMessage' => 'Vous ne pouvez télécharger que {{ limit }} images maximum',
+                    ]),
+                    new All([
+                        new File([
+                            'maxSize' => '2048k',
+                            'maxSizeMessage' => 'La taille maximale autorisée est de {{ limit }}.',
+                            'mimeTypes' => [
+                                'image/jpeg',
+                                'image/png'
+                            ],
+                            'mimeTypesMessage' => 'Veuillez télécharger une image valide (JPEG, PNG, GIF)',
+                        ])
+                    ])
+                ],
+            ])
+            ->add('description', TextType::class, [
+                'required' => false,
+            ])
+            ->add('url', UrlType::class, [
+                'label' => 'URL(facultatif)',
+                'required' => false,
+                'constraints' => [
+                    new Url(),
+                ],
+            ])
+            ->add('nameButton', TextType::class, [
+                'required' => false,
+            ])
+            ->add('Valider', SubmitType::class);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Slider::class,
+        ]);
+    }
+}
