a7d7887d3d9b7c4e514f4af7e9afd1fa56429331
anthony <anthonyspiteri@hotmail.fr>
2023-07-06 14:51:39 +0200

    feat: sliderController add and edit slider

--- src/Controller/SliderController.php
+++ src/Controller/SliderController.php
@@ -20,3 +20,69 @@ class SliderController extends AbstractController
             'controller_name' => 'SliderController',
         ]);
     }
+
+    #[Route('/slider/add', name: 'add_slide')] // ajouter un slide
+    #[Route('/slider/{id}/edit', name: 'edit_slide')] // modifier un slide
+    public function add(ManagerRegistry $doctrine, Slider $slider = null,  Request $request): Response
+    {
+
+        $entityManager = $doctrine->getManager(); // on récupère les ressources
+
+        if (!$slider) { // edit
+            $slider = new Slider();
+        } //add
+
+
+        $form = $this->createForm(SliderType::class, $slider,  []);
+        $form->handleRequest($request); // analyse the request
+
+
+        if ($form->isSubmitted() && $form->isValid()) { // valid respecter les contraintes
+            // on recupere les images transmisse
+            $images = $form->get('images')->getData();
+
+            foreach ($slider->getImage() as $image) {
+                $slider->removeImage($image);
+                $entityManager->remove($image); // Supprimer l'image de la base de données si nécessaire
+
+            }
+
+            // on boucle sur les tableaux
+            foreach ($images as $image) {
+                // on genere un nouveau nom de fichier
+                $fichier = md5(uniqid()) . '.' . $image->guessExtension();
+
+                //on copie le fichier dans le dossier uploads
+                $image->move(
+                    $this->getParameter('images_directory'),
+                    $fichier
+                );
+
+                //on stocke l'image dans la base de données
+                $img = new Images();
+                $img->setNomImage($fichier);
+                $slider->addImage($img);
+            }
+
+            $slider = $form->getData();
+
+            // $evenement->setUser($user); // install mon user
+            // $now = new \DateTime(); // objet date
+            //$evenement->setDateCreationevenement($now); // installe ma date
+
+
+
+            $entityManager->persist($slider); // on enregistre la ressource
+            $entityManager->flush(); // on envoie la ressource insert into
+
+            return $this->redirectToRoute('app_home');
+        }
+
+        return $this->render('slider/add.html.twig', [
+            'formAddSlider' => $form->createView(), // généré le visuel du form
+            "edit" => $slider->getId(),
+            "slider" => $slider,
+
+
+        ]);
+    }
