ff55aa68c88bbd96bf57437202c0d7484c245782
anthony <anthonyspiteri@hotmail.fr>
2023-07-12 13:46:30 +0200

    feat: form adress for order

---
+++ src/Form/OrderType.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Commande;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class OrderType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom', TextType::class, [
+                'label' => 'Nom',
+                'attr' => [
+                    'placeholder' => 'Entrez votre nom',
+                ],
+            ])
+            ->add('prenom', TextType::class, [
+                'label' => 'Prénom',
+                'attr' => [
+                    'placeholder' => 'Entrez votre prénom',
+                ],
+            ])
+            ->add('adresse', TextType::class, [
+                'label' => 'Adresse',
+                'attr' => [
+                    'placeholder' => 'Entrez votre adresse',
+                ],
+            ])
+            ->add('ville', TextType::class, [
+                'label' => 'Ville',
+                'attr' => [
+                    'placeholder' => 'Entrez votre ville',
+                ],
+            ])
+            ->add('codePostal', TextType::class, [
+                'label' => 'Code Postal',
+                'attr' => [
+                    'placeholder' => 'Entrez votre code postal',
+                ],
+            ])
+            ->add('Valider', SubmitType::class);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Commande::class,
+        ]);
+    }
+}
