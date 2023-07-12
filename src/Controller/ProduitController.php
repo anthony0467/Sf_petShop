7f805582e0a5e93e58ab7475bfffdca7429d9205
anthony <anthonyspiteri@hotmail.fr>
2023-07-12 13:48:35 +0200

    feat: showOrder function

--- src/Controller/ProduitController.php
+++ src/Controller/ProduitController.php
@@ -4,8 +4,11 @@ namespace App\Controller;
 
 
 use App\Entity\Produit;
+use App\Form\OrderType;
+use App\Entity\Commande;
 use App\Entity\Categorie;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -25,22 +28,61 @@ class ProduitController extends AbstractController
     {
         $user = $this->getUser();
         $vendeur = $produit->getUser();
-        
-        if($produit){
-            
-           // $message->setExpediteur($user);
+
+        if ($produit) {
+
+            // $message->setExpediteur($user);
             //$message->setDestinataire($vendeur);
 
-         return $this->render('produit/show.html.twig', [
-             'controller_name' => 'HomeController',
-             'produit' => $produit,
-             
-             
-             
-         ]);
-        }else{
+            return $this->render('produit/show.html.twig', [
+                'controller_name' => 'HomeController',
+                'produit' => $produit,
+
+
+
+            ]);
+        } else {
             return $this->redirectToRoute('app_home');
         }
+    }
 
+
+    #[Route('/produit/order/{id}', name: 'show_order')] // vue de la commande
+    public function showOrder(ManagerRegistry $doctrine,  Produit $produit = null, Request $request): Response
+    {
+        $user = $this->getUser();
+
+        if ($produit) {
+
+            $commande = new Commande;
+            $form = $this->createForm(OrderType::class, $commande,  []);
+            $form->handleRequest($request); // analyse the request
+
+            if ($form->isSubmitted() && $form->isValid()) { // valid respecter les contraintes
+
+                $commande = $form->getData();
+
+                //$commande->setUser($user); // install mon user
+                $now = new \DateTime(); // objet date
+                $commande->setDateCommande($now); // installe ma date
+                $commande->setCommander($user);
+                $entityManager = $doctrine->getManager(); // on récupère les ressources
+                $entityManager->persist($commande); // on enregistre la ressource
+                $entityManager->flush(); // on envoie la ressource insert into
+
+                return $this->redirectToRoute('app_home');
+            }
+
+
+
+            return $this->render('produit/order.html.twig', [
+                'controller_name' => 'HomeController',
+                'produit' => $produit,
+                'formCommande' => $form->createView(),
+
+            ]);
+        } else {
+            return $this->redirectToRoute('app_home');
+        }
     }
 }
