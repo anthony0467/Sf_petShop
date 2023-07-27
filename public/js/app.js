d773442b48dab29dafe778b7c8b73f900c197cfc
anthony <anthonyspiteri@hotmail.fr>
2023-07-27 11:30:26 +0200

    fix: script ajax for search operational

--- public/js/app.js
+++ public/js/app.js
@@ -1,25 +1,37 @@
+// requete ajax function recherche produit
 $(document).ready(function() {
     $('#search-form').submit(function(event) {
-        event.preventDefault(); // Empêche le rechargement de la page
-      console.log('Formulaire de recherche soumis !');
-      // Récupérer les données du formulaire
-    //   var formData = $(this).serialize()
-      // Envoyer les données du formulaire via AJAX à la route 'search_results'
-      $.ajax({
-        type: 'GET',
-        url: '{{ path("search_results") }}', // URL vers la route AJAX que vous avez créée
-        data: $(this).serialize(),
-        success: function(data) {
-            console.log(data)
-          // Mettre à jour la zone d'affichage des résultats avec les données reçues
-          $('#search-results').html(data.result);
-            // $('#search-results').empty().prepend(data.result)
-            
-         
-        },
-        error: function() {
-          console.log('Une erreur s\'est produite lors de la recherche.');
-        }
-      });
+        event.preventDefault();
+      
+        // Récupérer les données du formulaire
+        var mots = $('[name="search_produit[mots]"]').val();
+        var categorie = $('[name="search_produit[categorie]"]').val();
+    
+        // Construire l'URL de la requête AJAX avec les paramètres de recherche
+        var url = '/search?mots=' + encodeURIComponent(mots) + '&categorie=' + encodeURIComponent(categorie);
+        
+        // Envoyer les données du formulaire via AJAX à la route 'search_results'
+        $.ajax({
+            type: 'GET',
+            url: url,
+            data: $(this).serialize(),
+            dataType: 'json',
+            success: function(data) {
+                // Mettre à jour la zone d'affichage des résultats avec le contenu HTML
+                $('#search-results').html(data.result);
+
+                // Faire apparaître les éléments un par un avec un décalage
+                var searchResults = $('#search-results li').hide();
+                searchResults.each(function(index) {
+                    var $element = $(this);
+                    setTimeout(function() {
+                        $element.fadeIn(400);
+                    }, 350 * index);
+                });
+            },
+            error: function() {
+                console.log('Une erreur s\'est produite lors de la recherche.');
+            }
+        });
     });
-  });
+});
