38e0d2ce93859feb81e8a30cbf33d19e7806e170
anthony <anthonyspiteri@hotmail.fr>
2023-07-27 15:49:53 +0200

    feat: ajax for pagination

--- public/js/app.js
+++ public/js/app.js
@@ -4,11 +4,11 @@ $(document).ready(function() {
         event.preventDefault();
       
         // Récupérer les données du formulaire
-        var mots = $('[name="search_produit[mots]"]').val();
-        var categorie = $('[name="search_produit[categorie]"]').val();
+        let mots = $('[name="search_produit[mots]"]').val();
+        let categorie = $('[name="search_produit[categorie]"]').val();
     
         // Construire l'URL de la requête AJAX avec les paramètres de recherche
-        var url = '/search?mots=' + encodeURIComponent(mots) + '&categorie=' + encodeURIComponent(categorie);
+        let url = '/search?mots=' + encodeURIComponent(mots) + '&categorie=' + encodeURIComponent(categorie);
         
         // Envoyer les données du formulaire via AJAX à la route 'search_results'
         $.ajax({
@@ -21,9 +21,9 @@ $(document).ready(function() {
                 $('#search-results').html(data.result);
 
                 // Faire apparaître les éléments un par un avec un décalage
-                var searchResults = $('#search-results li').hide();
+                let searchResults = $('#search-results li').hide();
                 searchResults.each(function(index) {
-                    var $element = $(this);
+                    let $element = $(this);
                     setTimeout(function() {
                         $element.fadeIn(400);
                     }, 350 * index);
@@ -35,3 +35,39 @@ $(document).ready(function() {
         });
     });
 });
+
+
+// letiable pour vérifier si c'est la première mise à jour
+let isFirstLoad = true;
+
+// Requête AJAX pour la pagination des produits
+$(document).ready(function() {
+    $(document).on('click', '.pagination a', function(event) {
+        event.preventDefault();
+        let url = '/pagination?page=' + $(this).text();
+        console.log(url);
+        // Envoyer les données de la requête AJAX à la route 'app_pagination'
+        $.ajax({
+            type: 'GET',
+            url: url,
+            dataType: 'json',
+            success: function(data) {
+                console.log('data: ', data);
+                // Mettre à jour la zone d'affichage des résultats avec le contenu HTML
+                $('#article-container').fadeOut(400, function() {
+                    $(this).html(data.resultProduct).fadeIn(400);
+                });
+                console.log('result :', data.resultProduct)
+
+                // La première fois, on n'ajoute pas de fondu
+                if (isFirstLoad) {
+                    isFirstLoad = false;
+                }
+            },
+            error: function() {
+                console.log('Une erreur s\'est produite lors de la pagination.');
+            }
+        });
+    });
+});
+
