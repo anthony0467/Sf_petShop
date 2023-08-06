1de19e7575652d3399f16c29abbddcc152bbafcd
anthony <anthonyspiteri@hotmail.fr>
2023-08-06 18:21:52 +0200

    feat: function send offer

--- src/Controller/ProduitController.php
+++ src/Controller/ProduitController.php
@@ -3,7 +3,10 @@
 namespace App\Controller;
 
 
+use App\Entity\User;
+use App\Entity\Offre;
 use App\Entity\Produit;
+use App\Form\OffreType;
 use App\Form\OrderType;
 use App\Entity\Commande;
 use App\Entity\Categorie;
@@ -12,6 +15,8 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Mailer\MailerInterface;
 
 class ProduitController extends AbstractController
 {
@@ -27,7 +32,8 @@ class ProduitController extends AbstractController
     public function show(ManagerRegistry $doctrine,  Produit $produit = null,): Response
     {
         $user = $this->getUser();
-        $vendeur = $produit->getUser();
+
+
 
         if ($produit) {
 
@@ -84,5 +90,78 @@ class ProduitController extends AbstractController
         } else {
             return $this->redirectToRoute('app_home');
         }
+    }
+
+    #[Route('/offre/{vendorId}/{productId}', name: 'show_offre')] // formulaire offre 
+    public function send(Request $request, ManagerRegistry $doctrine, $vendorId, $productId): Response
+    {
+        $user = $this->getUser();
+        //$vendeur = $doctrine->getRepository(Produit::class)->find($vendorId);
+        //$vendor = $doctrine->getRepository(User::class)->find($vendorId);
+        $offre = new Offre();
+        $produit = $doctrine->getRepository(Produit::class)->find($productId);
+        $form = $this->createForm(OffreType::class, $offre);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $offre->setStatut(Offre::STATUT_EN_ATTENTE);
+            $offre->setUsers($user);
+            $offre->setNotifStatus(false);
+            $offre->setProduits($produit);
+            $offre->setDate(new \DateTime);
+
+
+            $em = $doctrine->getManager();
+            $em->persist($offre);
+            $em->flush();
+
+            $this->addFlash("offre", "Offre envoyé avec succès", "success");
+            return $this->redirectToRoute('show_home');
+        }
+
+        return $this->render('produit/offre.html.twig', [
+            "form" => $form->createView(),
+        ]);
+    }
+
+    #[Route('/changer-statut-offre/{id}/{statut}', name: 'changer_statut_offre')]
+    public function changerStatutOffre(ManagerRegistry $doctrine, Offre $offre, string $statut, MailerInterface $mailer): Response
+    {
+        // Vérifier que l'utilisateur connecté est bien le propriétaire de l'offre
+        if ($this->getUser() !== $offre->getProduits()->getUser()) {
+            throw $this->createAccessDeniedException();
+        }
+
+        // Mettre à jour le statut de l'offre en fonction de la valeur passée dans l'URL
+        switch ($statut) {
+            case 'acceptee':
+                $offre->setStatut(Offre::STATUT_ACCEPTEE);
+                $offre->setNotifStatus(true);
+                dump('Envoyer un e-mail ici');
+                // Envoi d'un e-mail de notification au client
+                $clientEmail = $offre->getUsers()->getEmail();
+                $produitNom = $offre->getProduits()->getNomProduit();
+
+                $email = (new Email())
+                    ->from('votre_adresse_email@example.com')
+                    ->to($clientEmail)
+                    ->subject('Votre offre a été acceptée')
+                    ->text("Félicitations, votre offre pour le produit $produitNom a été acceptée.");
+
+                $mailer->send($email);
+                break;
+            case 'refusee':
+                $offre->setStatut(Offre::STATUT_REFUSEE);
+                break;
+            default:
+                throw new \InvalidArgumentException("Statut invalide");
+        }
+
+        $entityManager = $doctrine->getManager();
+        $entityManager->persist($offre);
+        $entityManager->flush();
+
+        // Rediriger l'utilisateur vers la page du profil
+        return $this->redirectToRoute('show_home');
     }
 }
