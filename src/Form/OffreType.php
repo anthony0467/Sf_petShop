ca989bc9096f89d486760102ee835a8ed357ed9a
anthony <anthonyspiteri@hotmail.fr>
2023-08-06 18:20:14 +0200

    feat: offreType form

---
+++ src/Form/OffreType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Offre;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class OffreType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('prix')
+            //->add('date')
+            //->add('statut')
+            //->add('produits')
+            ->add('Valider', SubmitType::class);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Offre::class,
+        ]);
+    }
+}
