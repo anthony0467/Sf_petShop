4d6e86f7c8cbf5e064af845c262a97790b0d93ca
anthony <anthonyspiteri@hotmail.fr>
2023-09-13 11:51:16 +0200

    feat: contact entity and controller

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Contact;
+use App\Form\ContactType;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class ContactController extends AbstractController
+{
+    #[Route('/contact', name: 'app_contact')]
+    public function index(): Response
+    {
+        return $this->render('contact/index.html.twig', [
+            'controller_name' => 'ContactController',
+        ]);
+    }
+
+    #[Route('/contact/add', name: 'send_contact')]
+    public function send(Request $request, ManagerRegistry $doctrine): Response
+    {
+
+        $contact = new Contact();
+        $form = $this->createForm(ContactType::class, $contact);
+        $form->handleRequest($request);
+
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+
+            $em = $doctrine->getManager();
+            $em->persist($contact);
+            $em->flush();
+
+            $this->addFlash("message", "Message envoyé avec succès", "success");
+            return $this->redirectToRoute('app_home');
+        }
+
+        return $this->render('contact/add.html.twig', [
+            "form" => $form->createView(),
+        ]);
+    }
+
+    #[Route('/contact/show/{id}', name: 'show_contact')]
+    public function show(Contact $contact, ManagerRegistry $doctrine): Response
+    {
+
+        if (!$contact) {
+            return $this->redirectToRoute('app_home');
+        }
+
+        return $this->render('contact/show.html.twig', [
+            "contact" => $contact
+        ]);
+    }
+
+    #[Route('/contact/delete/{id}', name: 'delete_contact')]
+    public function delete(Contact $contact, ManagerRegistry $doctrine): Response
+    {
+
+        $em = $doctrine->getManager();
+        $em->remove($contact);
+        $em->flush();
+
+        return $this->redirectToRoute('show_admin');
+    }
+}
