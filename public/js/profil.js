65006d5ff4af5a2b51d6ac8a172ec0608e2e7d86
anthony <anthonyspiteri@hotmail.fr>
2023-10-02 15:11:47 +0200

    function read in read.js

--- public/js/profil.js
+++ public/js/profil.js
@@ -22,29 +22,4 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
 
-// notif read
 
-    
-    const notifications = document.querySelectorAll(".notification");
-    console.log('coucou')
-    notifications.forEach(notification => {
-      const notificationId = notification.getAttribute("data-notification-id");
-      const isDefaultNotification = notification.textContent.includes("Offre en cours de traitement");
-
-      notification.addEventListener("click", function () {
-        if (!notification.classList.contains("read") && !isDefaultNotification) {
-          // Envoyer une requête AJAX pour marquer l'offre comme lue
-          fetch(`/marquer-offre-lue/${notificationId}`, {
-            method: "POST",
-            headers: {
-              "X-Requested-With": "XMLHttpRequest",
-              "Content-Type": "application/json"
-            }
-          });
-
-          // Marquer la notification comme lue visuellement
-          notification.classList.remove("bold");
-        }
-      });
-    });
- 
